de833bae524fdc06f7a2b5cef49836f410885981
Michal Mrowicki <mrg.mrowicki@gmail.com>
2026-07-26 13:48:14 +0200

    generating LinkedList view files and attaching its dependencies to other files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home';
 import { StackComponent } from './views/stack/stack';
 import { QueueComponent } from './views/queue/queue';
+import { LinkedListComponent } from './views/linkedList/linkedList';
 
 export const routes: Routes = [
     {path : '', component: HomeComponent},
     {path : 'stack', component: StackComponent},
     {path : 'queue', component: QueueComponent},
+    {path: 'linkedList', component: LinkedListComponent}
 ];
